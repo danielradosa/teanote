baf3a7e60f80b4c39c192d1c968c3b629fca146f
Daniel Radosa <28@danielradosa.com>
2025-10-26 22:32:33 +0100

    chore: install deps link files

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="app-container">
+      <h1>🍵 teanote running</h1>
+    </div>
+  );
+};
+
+export default App;
