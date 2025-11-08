a226d7b761f1e31bdd699011737c616b91c054f3
Daniel Radosa <28@danielradosa.com>
2025-11-08 22:45:48 +0100

    come on fr

--- client/src/components/payment/SubscribeMonthly.tsx
+++ client/src/components/payment/SubscribeMonthly.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { MoonLoader } from 'react-spinners';
 import { useAuthStore } from '../../stores/useAuthStore';
 
